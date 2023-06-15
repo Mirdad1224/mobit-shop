d3e61b14d7c9ed13e38db591ff714854dafee7a1
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-15 12:12:06 +0330

    create a utility function for loading fonts

---
+++ utils/font.ts
@@ -0,0 +1,99 @@
+import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const IranSans = localFont({
+  src: [
+    {
+      path: '../public/fonts/IRANSansWeb_Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Black.woff',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Bold.woff2',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Bold.woff',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Bold.ttf',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Medium.woff',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Light.woff',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_UltraLight.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_UltraLight.woff',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb_UltraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb.woff2',
+      weight: 'normal',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb.woff',
+      weight: 'normal',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/IRANSansWeb.ttf',
+      weight: 'normal',
+      style: 'normal',
+    },
+  ],
+});
