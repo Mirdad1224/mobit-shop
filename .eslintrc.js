e5ab228acdfa9258a3bba5b610bb60bf8ec0644c
Mirdad1224 <ali.mirdad@yahoo.com>
2023-06-15 12:09:48 +0330

    refine eslint config for the better supporting of storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,74 +1,70 @@
 module.exports = {
-   root: true,
-   parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
-      ecmaFeatures: {
-         jsx: true,
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
       },
-   },
-
-   env: {
-      browser: true,
-      node: true,
-      es6: true,
-   },
-
-   settings: {
-      react: {
-         version: "detect",
+    },
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'prettier',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:security/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
       },
-      "import/resolver": {
-         node: {
-            extensions: [".ts", ".tsx"],
-         },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
-   },
-
-   plugins: ["@typescript-eslint"],
-   extends: [
-      "next/core-web-vitals",
-      "plugin:@typescript-eslint/recommended",
-      "airbnb",
-      "prettier",
-      "plugin:jsx-a11y/recommended",
-      "plugin:prettier/recommended",
-      "plugin:sonarjs/recommended",
-      "plugin:security/recommended",
-      "plugin:react-hooks/recommended",
-      "next/core-web-vitals",
-   ],
-
-   rules: {
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/no-explicit-any": "error",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-filename-extension": [
-         1,
-         {
-            extensions: [".ts", ".tsx", ".js", ".jsx"],
-         },
-      ],
-      "react/jsx-props-no-spreading": "off",
-      "import/extensions": [
-         "error",
-         "ignorePackages",
-         {
-            js: "never",
-            jsx: "never",
-            ts: "never",
-            tsx: "never",
-         },
-      ],
-      "jsx-a11y/anchor-is-valid": [
-         "error",
-         {
-            components: ["Link"],
-            specialLink: ["hrefLeft", "hrefRight"],
-            aspects: ["invalidHref", "preferButton"],
-         },
-      ],
-      "no-nested-ternary": "off",
-      "import/prefer-default-export": "off",
-   },
+    ],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    'no-nested-ternary': 'off',
+    'import/prefer-default-export': 'off',
+  },
 };
